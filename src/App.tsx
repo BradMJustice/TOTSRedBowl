d020da43eda2dae0b362b06ec55cf74a62f6d1bf
Bradley Justice <bjustice@buxtonco.com>
2021-02-06 12:40:36 -0600

    Added date override

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { GoogleSpreadsheet } from 'google-spreadsheet';
-import { RedBowlResponse } from './Models/Reponse';
 import { Col, Row } from 'antd';
-import styled from 'styled-components';
-import random from 'random';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 import moment from 'moment';
+import random from 'random';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { RedBowlResponse } from './Models/Reponse';
 
 const debug = false;
-
-const cleanResponses = (responses: RedBowlResponse[]): RedBowlResponse[] => {
-
-	//Filter responses to those submitted after 1 hour before curtain
-	const todayMoment = moment(new Date());
-	const dayOfWeek = todayMoment.day();
-	let minHour: number = 23;
-
-	if (debug) {
-		minHour = 0;
-	} else if (dayOfWeek === 5 || dayOfWeek === 6) {
-		minHour = 18;
-	} else if (dayOfWeek === 0) {
-		minHour = 13;
-	}
-
-	const timeFilteredResponses = responses.filter((r) => {
-		const responseMoment = moment(r.date);
-		const responseHour = responseMoment.hour();
-
-		const isSameDay = responseMoment.isSame(todayMoment, 'day');
-		const isAfterMinHour = responseHour >= minHour
-
-		return isSameDay && isAfterMinHour;
-	});
-
-	const dedupedResponses: RedBowlResponse[] = [];
-
-	for (const response of timeFilteredResponses) {
-		const existingResponse = dedupedResponses.find((r) => {
-			return r.email === response.email;
-		});
-
-		if (!existingResponse) {
-			dedupedResponses.push(response);
-		}
-	}
-
-	return dedupedResponses;
-};
 
 const CenteredCol = styled(Col)`
 	text-align: center;
 `;
+
+const dateToString = (date: Date): string => {
+
+	const year = date.getFullYear().toString();
+	let month = (date.getMonth() + 1).toString();
+	if (month.length === 1) {
+		month = `0${month}`;
+	}
+	let day = date.getDate().toString();
+	if (day.length === 1) {
+		day = `0${day}`;
+	}
+
+	return `${year}-${month}-${day}`;
+}
 
 function App() {
 
 	const [doc, setDoc] = useState<GoogleSpreadsheet>();
 	const [responses, setResponses] = useState<RedBowlResponse[]>([]);
 	const [winnerIndex, setWinnerIndex] = useState<number>(-1);
+	const [filterDate, setFilterDate] = useState<string>(dateToString(new Date()))
 
 	useEffect(() => {
 		setDoc(new GoogleSpreadsheet('1OEry52CfvOobIL9hjQGPOCl0ncIMF_KHrtY4glougLE'));
@@ -81,8 +57,7 @@ function App() {
 								return returnResponse;
 							});
 
-							const cleaned = cleanResponses(parsedResponses);
-							setResponses(cleaned);
+							setResponses(parsedResponses);
 						}).catch((error) => {
 							console.log(error);
 						});
@@ -92,24 +67,84 @@ function App() {
 		}
 	}, [doc]);
 
+	const cleanResponses = (responses: RedBowlResponse[]): RedBowlResponse[] => {
+
+		//Filter responses to those submitted after 2 hours before curtain
+		const dateMoment = moment(filterDate);
+		
+		const dayOfWeek = dateMoment.day();
+		let minHour: number = 23;
+
+		if (debug) {
+			minHour = 0;
+		} else if (dayOfWeek === 5 || dayOfWeek === 6) {
+			minHour = 17;
+		} else if (dayOfWeek === 0) {
+			minHour = 12;
+		}
+
+		const timeFilteredResponses = responses.filter((r) => {
+			const responseMoment = moment(r.date);
+			const responseHour = responseMoment.hour();
+
+			const isSameDay = responseMoment.isSame(dateMoment, 'day');
+			const isAfterMinHour = responseHour >= minHour
+
+			return isSameDay && isAfterMinHour;
+		});
+
+		const dedupedResponses: RedBowlResponse[] = [];
+
+		for (const response of timeFilteredResponses) {
+			const existingResponse = dedupedResponses.find((r) => {
+				return r.email === response.email;
+			});
+
+			if (!existingResponse) {
+				dedupedResponses.push(response);
+			}
+		}
+
+		return dedupedResponses;
+	};
+
 	const generateRandomNumber = () => {
-		const randomIndex = random.int(0, responses?.length - 1);
+		const cleaned = cleanResponses(responses);
+		const randomIndex = random.int(0, cleaned?.length - 1);
 		console.log(randomIndex);
 		setWinnerIndex(randomIndex);
 	}
 
-	const responseList = responses?.map((response) => {
+	const onDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setFilterDate(event.target.value);
+	}
+
+	const cleaned = cleanResponses(responses);
+	const responseList = cleaned?.map((response) => {
 		return <li key={response.email}>
 			{response.email};
 		</li>
 	});
 
-	const winner = winnerIndex !== -1 ? responses[winnerIndex] : null;
+	const winner = winnerIndex !== -1 ? cleaned[winnerIndex] : null;
 	const winnerName = `${winner?.firstname} ${winner?.lastname}`;
 	const winnerEmail = `${winner?.email}`;
 
 	return (
 		<div className="App">
+			<Row>
+				<CenteredCol>
+					<h3>Date Override</h3>
+					<p>
+						<input
+							type="date"
+							style={{ width: "200px" }}
+							value={filterDate}
+							onChange={onDateChange}
+						/>
+					</p>
+				</CenteredCol>
+			</Row>
 			<Row>
 				<CenteredCol span={24}>
 					{
